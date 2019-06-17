de70f2f9d677a5768686249978156b9ae424b4ae
Chandon Porter <cneilporter94@gmail.com>
2019-06-17 23:37:07 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,44 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  marray = ['Chandon', 'Cherian', 'Chanell', 'Corey']
 end
 
 def add_element_to_end_of_array(array, element)
+  array = ["wow", "I", "am", "really", "learning"]
+  array.push ('arrays!')
   
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array = ["I", "am", "really", "learning"]
+  array.unshift ('wow')
 end
 
 def remove_element_from_end_of_array(array)
+  array = "arrays!"
   
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array = "wow"
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array = "am"
+    
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array = "wow"
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array = "arrays!"
 end
 
 def update_element_from_index(array, index_number, element)
-
+array = "totally"
 end
