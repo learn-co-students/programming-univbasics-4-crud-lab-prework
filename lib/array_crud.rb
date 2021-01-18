4566c8d00cd6286a7b741844ea33200513c27dc0
Bleak Chandler <bleakchandler@gmail.com>
2021-01-18 23:18:37 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,49 +3,49 @@ def create_an_empty_array
 end
 
 def create_an_array
-  newarray = ["Mario", "Link", "Kirby", "Samus"]
+  newarray = ["wow", "I", "am", "really"]
   
 end
 
 def add_element_to_end_of_array(array, element)
-   newarray = ["Mario", "Link", "Kirby", "Samus"]
+   newarray = ["wow", "I", "am", "really", "learning"]
    newarray.push "arrays!"
   
 end
 
 def add_element_to_start_of_array(array, element)
-   newarray = ["Mario", "Link", "Kirby", "Samus"]
+   newarray = ["wow", "I", "am", "really", "learning"]
    newarray.unshift "wow"
 end
 
 def remove_element_from_end_of_array(array)
-   newarray = ["Mario", "Link", "arrays!", "Samus"]
+   newarray = ["wow", "I", "am", "arrays!", "learning"]
    revisedarray = newarray.pop
-   newarray [2]
+   newarray [3]
 end
 
 def remove_element_from_start_of_array(array)
-   newarray = ["Mario", "wow", "Kirby", "Samus"]
+   newarray = ["I", "wow", "I'm", "really", "learning"]
    revisedarray = newarray.shift
      newarray [0]
 end
 
 def retrieve_element_from_index(array, index_number)
-   newarray = ["Mario", "wow", "am", "Samus"]
+   newarray = ["wow", "I", "am", "really", "learning"]
    newarray [2]
 end
 
 def retrieve_first_element_from_array(array)
-   newarray = ["wow", "Link", "Kirby", "Samus"]
+   newarray = ["wow", "I", "am", "really", "learning"]
    newarray [0]
 end
 
 def retrieve_last_element_from_array(array)
-   newarray = ["Mario", "Link", "Kirby", "arrays!"]
+   newarray = ["wow", "I'm", "learning", "arrays!"]
    newarray [3]
 end
 
 def update_element_from_index(array, index_number, element)
- newarray = ["Mario", "Link", "Kirby", "Samus", "King DeDeDe"]
+ newarray = ["wow", "I", "am", "really", "learning"]
  newarray[4] = "totally"
 end
